236c82f09b27fba6a4ac507ba74e532cb99ccbc2
Michel Albert <michel@albert.lu>
2017-08-20 14:10:30 +0000

    Make sure it also works on Raspian "stretch"

--- bootstrap.bash
+++ bootstrap.bash
@@ -2,7 +2,14 @@
 
 set -e
 
-sntp -s time.google.com
+if [[ "$(lsb_release -r | cut -f 2)" == "8.0" ]]; then
+	sntp -s time.google.com
+elif [[ "$(lsb_release -r | cut -f 2)" == "9.1" ]]; then
+	timedatectl set-ntp true
+else
+	echo "This is currently only tested with Raspian 8.0 (jessie) and 9.1 (stretch)"
+	exit 1
+fi
 
 aptitude update
 aptitude -y upgrade
@@ -67,9 +74,19 @@ if [[ "$(lsb_release -r | cut -f 2)" == "8.0" ]]; then
 	    -D PYTHON_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython3.4m.so \
 	    -D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include \
 	    -D BUILD_EXAMPLES=ON ../
-elif [[ "$(lsb_release -r | cut -f 2)" == "9.0" ]]; then
-	echo "Not yet implemented"
-	exit 1
+elif [[ "$(lsb_release -r | cut -f 2)" == "9.1" ]]; then
+	cmake \
+	    -D CMAKE_BUILD_TYPE=Release \
+	    -D CMAKE_INSTALL_PREFIX=/usr/local \
+	    -D INSTALL_C_EXAMPLES=OFF \
+	    -D INSTALL_PYTHON_EXAMPLES=ON \
+	    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+	    -D PYTHON3_EXECUTABLE=/usr/bin/python3 \
+	    -D PYTHON_INCLUDE_DIR=/usr/include/python3.5m \
+	    -D PYTHON_INCLUDE_DIR2=/usr/include/arm-linux-gnueabihf/python3.5m \
+	    -D PYTHON_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython3.5m.so \
+	    -D PYTHON3_NUMPY_INCLUDE_DIRS=/usr/lib/python3/dist-packages/numpy/core/include \
+	    -D BUILD_EXAMPLES=ON ../
 else
 	echo "This is currently only tested with Raspian 8.0 (jessie)"
 	exit 1
