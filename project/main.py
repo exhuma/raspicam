896c1d0bc5a29e238127ec089209c60107097f12
Michel Albert <michel@albert.lu>
2017-08-20 14:39:49 +0000

    Whitespace cleanup

--- project/main.py
+++ project/main.py
@@ -11,14 +11,14 @@ app = Flask(__name__)
 def multipart_stream(frame_generator):
     """
     Wrap each item from a generator with HTTP Multipart metadata. This is required for Motion-JPEG.
-    
+
     Example::
-    
+
         >>> frames = my_generator()
         >>> wrapped_generator = multipart_stream(frames)
         >>> for frame in wrapped_generator:
         ...     print(frame[:20])
-    
+
     :param frame_generator: A generater which generates image frames as *bytes* objects
     :return: A new, wrapped stream of bytes
     """
